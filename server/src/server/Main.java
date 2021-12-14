d28b7ba2a0d45c45c9bf6f231d1d0a18207d0c1f
AndreBertoDev <andre.bertogalli@outlook.it>
2021-12-14 17:58:36 +0100

    stsrt

---
+++ server/src/server/Main.java
@@ -0,0 +1,35 @@
+package server;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.sql.ResultSet;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+	
+		 try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	       	 Connection conn = null;
+	            conn = DriverManager.getConnection("jdbc:mysql://localhost/sailig_club","root", "");
+	            System.out.print("Database is connected !");
+	            
+	   		 Statement selectStmt = conn.createStatement();
+			 ResultSet rs = selectStmt.executeQuery("SELECT * FROM user;");
+			 
+			 ArrayList allRows = new ArrayList();
+			 int numberColumns = 7;
+			 System.out.print(rs.getString(0));
+	            conn.close();
+	        }
+	        catch(Exception e) {
+	            System.out.print("Do not connect to DB - Error:"+e);
+	        }
+		 
+
+	}
+}
