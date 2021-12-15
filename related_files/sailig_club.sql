8df691ba975526850e30a2440f237a1d1079d58b
AndreBertoDev <andre.bertogalli@outlook.it>
2021-12-15 17:04:56 +0100

    sql

---
+++ related_files/sailig_club.sql
@@ -0,0 +1,263 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Dic 14, 2021 alle 17:13
+-- Versione del server: 10.4.11-MariaDB
+-- Versione PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sailig_club`
+--
+CREATE DATABASE IF NOT EXISTS `sailig_club` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sailig_club`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `bank_transfer`
+--
+
+CREATE TABLE `bank_transfer` (
+  `iban` varchar(30) NOT NULL,
+  `bank` varchar(32) NOT NULL,
+  `id_member` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `boat`
+--
+
+CREATE TABLE `boat` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `length` double NOT NULL,
+  `id_member` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `boat_storage_fee`
+--
+
+CREATE TABLE `boat_storage_fee` (
+  `id` int(11) NOT NULL,
+  `payment_date` date NOT NULL,
+  `expiration_date` date NOT NULL,
+  `amount` double NOT NULL,
+  `id_boat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `credit_card`
+--
+
+CREATE TABLE `credit_card` (
+  `card_number` varchar(16) NOT NULL,
+  `cvv` int(11) NOT NULL,
+  `expiration_date` date NOT NULL,
+  `id_member` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `membership_fee`
+--
+
+CREATE TABLE `membership_fee` (
+  `id` int(11) NOT NULL,
+  `payment_date` date NOT NULL,
+  `expiration_date` date NOT NULL,
+  `price` double NOT NULL,
+  `id_member` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `race`
+--
+
+CREATE TABLE `race` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `race_participation`
+--
+
+CREATE TABLE `race_participation` (
+  `id_member` varchar(32) NOT NULL,
+  `id_race` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `surname` varchar(32) NOT NULL,
+  `address` varchar(32) NOT NULL,
+  `fiscal_code` varchar(16) NOT NULL,
+  `user_type` varchar(16) NOT NULL,
+  `password` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `bank_transfer`
+--
+ALTER TABLE `bank_transfer`
+  ADD PRIMARY KEY (`iban`),
+  ADD KEY `fkMemberBankTransfer` (`id_member`);
+
+--
+-- Indici per le tabelle `boat`
+--
+ALTER TABLE `boat`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkMember` (`id_member`);
+
+--
+-- Indici per le tabelle `boat_storage_fee`
+--
+ALTER TABLE `boat_storage_fee`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkBoatFee` (`id_boat`);
+
+--
+-- Indici per le tabelle `credit_card`
+--
+ALTER TABLE `credit_card`
+  ADD PRIMARY KEY (`card_number`),
+  ADD KEY `fkMemberCreditCard` (`id_member`);
+
+--
+-- Indici per le tabelle `membership_fee`
+--
+ALTER TABLE `membership_fee`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkMemberFee` (`id_member`);
+
+--
+-- Indici per le tabelle `race`
+--
+ALTER TABLE `race`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indici per le tabelle `race_participation`
+--
+ALTER TABLE `race_participation`
+  ADD PRIMARY KEY (`id_member`,`id_race`),
+  ADD KEY `fkRace` (`id_race`),
+  ADD KEY `fkMemberRace` (`id_member`);
+
+--
+-- Indici per le tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`,`user_type`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `boat`
+--
+ALTER TABLE `boat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `boat_storage_fee`
+--
+ALTER TABLE `boat_storage_fee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `membership_fee`
+--
+ALTER TABLE `membership_fee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `race`
+--
+ALTER TABLE `race`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `bank_transfer`
+--
+ALTER TABLE `bank_transfer`
+  ADD CONSTRAINT `fkMemberBank` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `boat`
+--
+ALTER TABLE `boat`
+  ADD CONSTRAINT `fkMember` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `boat_storage_fee`
+--
+ALTER TABLE `boat_storage_fee`
+  ADD CONSTRAINT `fkBoatFee` FOREIGN KEY (`id_boat`) REFERENCES `boat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `credit_card`
+--
+ALTER TABLE `credit_card`
+  ADD CONSTRAINT `fkMemberCreditCard` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `membership_fee`
+--
+ALTER TABLE `membership_fee`
+  ADD CONSTRAINT `fkMemberFee` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `race_participation`
+--
+ALTER TABLE `race_participation`
+  ADD CONSTRAINT `fkMemberRace` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fkRace` FOREIGN KEY (`id_race`) REFERENCES `race` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
