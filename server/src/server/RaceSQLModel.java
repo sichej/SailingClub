31b02b8d7235d3bf2b9dddc6b68e4cf4fd79d1de
Edosicca <47154408+sichej@users.noreply.github.com>
2021-12-20 17:29:33 +0100

    RaceSQLModel

---
+++ server/src/server/RaceSQLModel.java
@@ -0,0 +1,44 @@
+package server;
+import java.time.LocalDate;
+
+public class RaceSQLModel implements SQLModel{
+	private Integer id;
+	private LocalDate data;
+	private Double price;
+	
+	public RaceSQLModel(int id, LocalDate data, Double price) {
+		this.id = id;
+		this.data = data;
+		this.price = price;
+	}
+	
+	@Override
+	public String[] getSQLAttributes() {
+		return new String[]{"id", "data", "price"};
+	}
+
+	@Override
+	public String getSQLTableName() {
+		return "race";
+	}
+
+	@Override
+	public String[] getSQLValues() {
+		return new String[]{this.id.toString(), this.data.toString(), this.price.toString()};
+	}
+
+	@Override
+	public Class<?>[] getSQLTypes() {
+		return new Class[]{this.id.getClass(), this.data.getClass(), this.price.getClass()};
+	}
+
+	@Override
+	public Object getSQLPrimaryKeyValue() {
+		return this.id;
+	}
+
+	@Override
+	public String getSQLPrimaryKeyName() {
+		return "id";
+	}
+}
