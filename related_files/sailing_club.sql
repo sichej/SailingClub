30351f521e21e80bbdbb68c13fcab6a39e8c66f6
AndreBertoDev <andre.bertogalli@outlook.it>
2022-01-29 13:36:26 +0100

    db up

--- related_files/sailing_club.sql
+++ related_files/sailing_club.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 22, 2022 alle 19:27
+-- Creato il: Gen 29, 2022 alle 13:35
 -- Versione del server: 10.4.22-MariaDB
 -- Versione PHP: 8.1.1
 
@@ -33,6 +33,14 @@ CREATE TABLE `bank_transfer` (
   `id_member` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `bank_transfer`
+--
+
+INSERT INTO `bank_transfer` (`iban`, `bank`, `id_member`) VALUES
+('IT69X0300203280748853977885', 'Intesa san paolo', 'edo'),
+('IT92N0300203280851278978589', 'Credit agricole', 'edo');
+
 -- --------------------------------------------------------
 
 --
@@ -58,7 +66,10 @@ INSERT INTO `boat` (`id`, `name`, `length`, `id_member`, `picture`) VALUES
 (15, 'asdfghjklò', 456, 'edo', 'generic.jpg'),
 (16, 'wsdfgh66', 988, 'edo', 'generic.jpg'),
 (18, 'ASDFGH', 444, 'edo', 'generic.jpg'),
-(19, 'sadfghj', 32, 'edo', 'generic.jpg');
+(19, 'sadfghj', 32, 'edo', 'generic.jpg'),
+(21, 'jdiismdiamdismmi', 988, 'edo', 'generic.jpg'),
+(22, 'uu8888', 988, 'edo', 'generic.jpg'),
+(32, 'barca a remi', 3, 'edo', 'barcaaremi.jpg');
 
 -- --------------------------------------------------------
 
@@ -79,13 +90,16 @@ CREATE TABLE `boat_storage_fee` (
 --
 
 INSERT INTO `boat_storage_fee` (`id`, `payment_date`, `expiration_date`, `amount`, `id_boat`) VALUES
-(10, '2022-01-02', '2022-01-28', 132, 12),
-(11, '2022-01-02', '2022-01-21', 88, 13),
+(10, '2022-01-28', '2031-01-28', 132, 12),
+(11, '2022-01-28', '2024-01-21', 88, 13),
 (12, '2022-01-03', '2022-01-28', 90, 14),
 (13, '2022-01-02', '2022-01-27', 567, 15),
 (14, '2022-01-17', '2022-01-28', 5678, 16),
 (16, '2022-01-26', '2022-01-31', 67, 18),
-(17, '2022-01-02', '2022-01-31', 78, 19);
+(17, '2022-01-02', '2022-01-31', 78, 19),
+(19, '2022-01-02', '2022-01-31', 78, 21),
+(20, '2022-01-03', '2022-01-29', 9090, 22),
+(25, '2022-01-27', '2023-01-27', 19.5, 32);
 
 -- --------------------------------------------------------
 
@@ -99,6 +113,14 @@ CREATE TABLE `credit_card` (
   `expiration_date` date NOT NULL,
   `id_member` varchar(32) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `credit_card`
+--
+
+INSERT INTO `credit_card` (`card_number`, `cvv`, `expiration_date`, `id_member`) VALUES
+('4151839906197223', 354, '2026-01-09', 'edo'),
+('5061997095036317', 123, '2026-01-21', 'edo');
 
 -- --------------------------------------------------------
 
@@ -119,8 +141,8 @@ CREATE TABLE `membership_fee` (
 --
 
 INSERT INTO `membership_fee` (`id`, `payment_date`, `expiration_date`, `price`, `id_member`) VALUES
-(1, '2022-01-03', '2022-01-04', 78, 'Berto'),
-(2, '2022-01-18', '2025-01-17', 150, 'edo');
+(1, '2022-01-03', '2022-01-04', 599.99, 'Berto'),
+(2, '2022-01-29', '2037-01-17', 599.99, 'edo');
 
 -- --------------------------------------------------------
 
@@ -131,17 +153,19 @@ INSERT INTO `membership_fee` (`id`, `payment_date`, `expiration_date`, `price`, 
 CREATE TABLE `race` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `price` double NOT NULL
+  `price` double NOT NULL,
+  `name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dump dei dati per la tabella `race`
 --
 
-INSERT INTO `race` (`id`, `date`, `price`) VALUES
-(1, '2024-10-24', 200),
-(2, '0000-00-00', 200),
-(3, '2024-10-24', 200);
+INSERT INTO `race` (`id`, `date`, `price`, `name`) VALUES
+(1, '2016-06-09', 200, 'coppa del mondo'),
+(2, '2022-01-11', 200, 'coppa america'),
+(3, '2024-10-24', 200, 'coppa asia'),
+(4, '2016-01-07', 123, 'gara che è finita ');
 
 -- --------------------------------------------------------
 
@@ -151,8 +175,17 @@ INSERT INTO `race` (`id`, `date`, `price`) VALUES
 
 CREATE TABLE `race_participation` (
   `id_member` varchar(32) NOT NULL,
-  `id_race` int(11) NOT NULL
+  `id_race` int(11) NOT NULL,
+  `id_boat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `race_participation`
+--
+
+INSERT INTO `race_participation` (`id_member`, `id_race`, `id_boat`) VALUES
+('edo', 1, 14),
+('edo', 3, 13);
 
 -- --------------------------------------------------------
 
@@ -228,9 +261,8 @@ ALTER TABLE `race`
 -- Indici per le tabelle `race_participation`
 --
 ALTER TABLE `race_participation`
-  ADD PRIMARY KEY (`id_member`,`id_race`),
-  ADD KEY `fkRace` (`id_race`),
-  ADD KEY `fkMemberRace` (`id_member`);
+  ADD PRIMARY KEY (`id_member`,`id_race`,`id_boat`) USING BTREE,
+  ADD KEY `fk_race` (`id_race`,`id_member`,`id_boat`) USING BTREE;
 
 --
 -- Indici per le tabelle `user`
@@ -246,13 +278,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT per la tabella `boat`
 --
 ALTER TABLE `boat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT per la tabella `boat_storage_fee`
 --
 ALTER TABLE `boat_storage_fee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT per la tabella `membership_fee`
@@ -264,7 +296,7 @@ ALTER TABLE `membership_fee`
 -- AUTO_INCREMENT per la tabella `race`
 --
 ALTER TABLE `race`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Limiti per le tabelle scaricate
@@ -304,8 +336,9 @@ ALTER TABLE `membership_fee`
 -- Limiti per la tabella `race_participation`
 --
 ALTER TABLE `race_participation`
-  ADD CONSTRAINT `fkMemberRace` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fkRace` FOREIGN KEY (`id_race`) REFERENCES `race` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_member_race` FOREIGN KEY (`id_member`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `fk_race` FOREIGN KEY (`id_race`) REFERENCES `race` (`id`),
+  ADD CONSTRAINT `fk_race_boat` FOREIGN KEY (`id_boat`) REFERENCES `boat` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
